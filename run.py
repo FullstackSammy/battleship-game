19e47089231f68ac07dc0075cd7e9a6ff4b09739
Samuel Näsström <nasstromsamuel@icloud.com>
2022-10-13 12:59:11 +0000

    Defined the functions needed for the game

--- run.py
+++ run.py
@@ -1,6 +1,28 @@
 hidden_board = [[' '] * 8 for x in range(8)]
-#This creates a board with 8 rows and columns
+# This creates a board with 8 rows and columns
 guessing_board = [[' '] * 8 for x in range(8)]
-#This creates a board with 8 rows and columns
+# This creates a board with 8 rows and columns
 
-letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 
+                      'F': 5, 'G': 6, 'H': 7}
+# This converts letters into numbers
+
+def board():
+    pass
+
+
+def create_ships():
+    pass
+
+def get_ship_location():
+    """
+    Asks the user for what row and column they want to guess where the ship is
+    """
+    
+    pass
+
+def count_hit_ships():
+    """
+    This will count each hit you get. If you get all five
+    The game is over.
+    """
