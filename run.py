94a72d26d8989c93499abed1df585bce889ee515
Samuel Näsström <nasstromsamuel@icloud.com>
2022-10-13 12:42:57 +0000

    Add boards and letter to number converter

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+hidden_board = [[' '] * 8 for x in range(8)]
+#This creates a board with 8 rows and columns
+guessing_board = [[' '] * 8 for x in range(8)]
+#This creates a board with 8 rows and columns
+
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
