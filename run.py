72ad3e78007955486723fb1232aeb7df6f6525a1
Samuel Näsström <nasstromsamuel@icloud.com>
2022-10-15 10:59:00 +0000

    add legend

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 from random import randint
 
+# Legend
+# "@" for placing ship
+# " " for available space
+# "X" for battleships hit
+# "-" for battleships missed
+
 # This creates a board with 8 rows and columns
 computer_board = [[" "] * 8 for x in range(8)]
 # This creates a board with 8 rows and columns
 guessing_board = [[" "] * 8 for i in range(8)]
+# This creates a board with 8 rows and columns
+player_board = [[" "] * 8 for i in range(8)]
 
 
 # This converts letters into numbers
@@ -21,13 +29,14 @@ letters_to_numbers = {
 
 def print_board(board):
     """
-    This function prints and displays the boards
+    This function prints and displays the boards.
+    numbers for rows and letters for columns
     """
     print("  A B C D E F G H")
     print("  +-+-+-+-+-+-+-+")
     row_number = 1
     for row in board:
-        print("%d|%s|" % (row_number, "|".join(row)))
+        print(row_number, "|".join(row))
         row_number += 1
 
 
@@ -76,20 +85,21 @@ def run_game():
     """
     create_ships(computer_board)
     turns = 10
+    print("Welcome to Battleships")
     while turns > 0:
-        print('Guess a battleship location')
+        print('Guess a location for enemy ship')
         print_board(guessing_board)
         row, column = get_ship_location()
         if guessing_board[row][column] == "-":
             print("That guess has already been made")
         elif guessing_board[row][column] == "X":
             print("Well done! You sunk an enemy ship!")
-            guessing_board[row][column] = "X" 
-            turns -= 1  
+            guessing_board[row][column] = "X"
+            turns -= 1
         else:
             print("Aw, you missed. try again!")
-            guessing_board[row][column] = "-"   
-            turns -= 1     
+            guessing_board[row][column] = "-"
+            turns -= 1
         if count_hit_ships(guessing_board) == 5:
             print("Congratulations! You win the game!")
             break
